39dd791b804381f88e3e6589baafb517c14f3bc6
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-06-14 04:57:08 -0400

    here

---
+++ function_template.py
@@ -0,0 +1,8 @@
+def codeFunctionHere():
+
+    return "Hooray! I've coded a function"
+
+
+if "__name__" == "__main__":
+    # why above? name is the 'name' designated for what is equal to the name of what comes after 'python' on the command line
+    codeFunctionHere()
