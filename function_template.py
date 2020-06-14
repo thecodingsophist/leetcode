c20e7b72003ea02389169e80c4d69d4ca84f5969
Zhanxi Ni <ni.zhanxi@gmail.com>
2020-06-14 05:37:59 -0400

    template now works

--- function_template.py
+++ function_template.py
@@ -1,8 +1,8 @@
 def codeFunctionHere():
 
-    return "Hooray! I've coded a function"
+    return "Hooray! I have coded a function"
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     # why above? name is the 'name' designated for what is equal to the name of what comes after 'python' on the command line
-    codeFunctionHere()
+    print(codeFunctionHere())
